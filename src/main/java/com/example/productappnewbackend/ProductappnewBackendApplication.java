0f20668b9b1187c9f6503ac8d3b225a76dfea172
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-06 15:26:14 +0530

    first commit

---
+++ src/main/java/com/example/productappnewbackend/ProductappnewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.productappnewbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductappnewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductappnewBackendApplication.class, args);
+	}
+
+}
